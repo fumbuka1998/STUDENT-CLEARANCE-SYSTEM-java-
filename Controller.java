24a3f2a7b52cd5d2654a3fa392f5f974c5878aad
#papa_python <limbufumbuka@gmail.com>
2020-08-20 15:38:27 +0300

    Add files via upload
    some of the codes of student clearance system
    -the student information page
    -the admin panel 
    -controller of the code and the main

---
+++ Controller.java
@@ -0,0 +1,13 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
+public class Controller {
+
+
+    @FXML protected void handleSubmitButtonAction(ActionEvent event){
+
+    }
+}
